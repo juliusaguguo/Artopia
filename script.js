2d4855d51c39f7eeaba93fe0f75b2e978595802d
Julius Aguguo <juliusaguguo@gmail.com>
2024-12-13 01:08:32 +0100

    updated the login page

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-const newsletter = document.getElementById("newsletter");
-const newsletterBtn = document.getElementById("newsletterBtn");
 
-newsletterBtn.addEventListener("click", () => {
-  if (
-    newsletter.value.includes("@") &&
-    newsletter.value.includes("gmail.com")
-  ) {
-    newsletter.value = " ";
-  } else {
-    alert("Both '@' & '.com' are required");
+/* === THIS IS THE VALIDATION FOR THE INDIVIDUAL ACCOUNT CREATION AND FOR THE BUSINESS ACCOUNT === */
+document.getElementById("register").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const fullname = document.getElementById("fullname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+  if (!fullname) {
+    alert("please enter your full name.");
+    return;
   }
+  if (!email) {
+    alert("please enter a valid email");
+    return;
+  }
+  if (!passwordRegex.test(password)) {
+    alert(
+      "password must be at least 6 characters long and include an uppercase letter, a lowercase letter, a number, and a symbol"
+    );
+    return;
+  }
+
+  window.location.href = "completeProfileForBusiness.html";
 });
 
-const footerEmailIcon = document.getElementById("footerEmailIcon");
-const footerEmail = document.getElementById("footerEmail");
 
-footerEmailIcon.addEventListener("click", () => {
-  if (
-    footerEmail.value.includes("@") &&
-    footerEmail.value.includes("gmail.com")
-  ) {
-    footerEmail.value = " ";
+
+
+/* == TO SHOW THE INPUT VALUE OF THE PASSWORD  == */
+document.getElementById("show").addEventListener("click", function () {
+  const passwordInput = document.getElementById("password");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    this.textContent = "Hide";
   } else {
-    alert("Both '@' & '.com' are required");
+    passwordInput.type = "password";
+    this.textContent = "Show";
   }
 });
-
-
-
- 
